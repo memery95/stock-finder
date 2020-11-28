1e5ee95ad0761d43f022c5892fb86f101341f7ac
Marcus Emery <marcusemery95@gmail.com>
2020-11-28 16:41:53 -0500

    made the google chart api ressponsive

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//Global Variable
+let currentValue = '';
+let currentData;
+
 function formatNewsQueryParams (params) {
     const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${params[key]}`);
     return queryItems.join('&');
@@ -48,51 +52,38 @@ function displayNewsResults (newsDataJson) {
 
     for (let i = 0; i < newsArray.length; i++) {
         $('#js-news-results').append(
-            `<a href='${newsArray[i].url}'><h3>${newsArray[i].title}</h3></a>
-            <p>${newsArray[i].summary}</p>`
+            `<hr><li>
+                <h3><a href='${newsArray[i].url}'>${newsArray[i].title}</a></h3>
+                <p maxlength='100'>${newsArray[i].summary}</p>
+            </li>`
         )
     }
 }
-
-//Stock Stats 
-//Open
-//High
-//Low
-//52 wk High 
-//52 wk low
-//Volume
-//Mkt Cap
 
 //Stock API info
 
 //Format stock symbol query 
 function formatStockQueryParams (params) {
-    
     const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${params[key]}`);
-    
     return queryItems.join('&');
 }
 
-
 //Get Stock symbol function
 function getStockData (searchSymbol) {
-    const stockURL = `https://www.alphavantage.co/query?`;
-    
-    //create parameters object
-    const params = {
-        function : 'TIME_SERIES_DAILY_ADJUSTED',
-        symbol : searchSymbol,
-        apikey : '2ROPO489EWWVNW2W'
-    }
-
-    //connecting params object to the formatStockQueryParans function
-    const queryString = formatStockQueryParams(params);
-    
-    //combining the url to the query params
-    const stockQueryURL = `${stockURL}${queryString}`;
-
     //fetch request for stock API
-    fetch(stockQueryURL)
+    console.log(currentData == null);
+    if (searchSymbol != currentValue || currentData == null) {
+        const params = {
+            function : 'TIME_SERIES_DAILY_ADJUSTED',
+            symbol : searchSymbol,
+            apikey : '2ROPO489EWWVNW2W'
+        }
+        const stockURL = `https://www.alphavantage.co/query?`;
+        //connecting params object to the formatStockQueryParans function
+        const queryString = formatStockQueryParams(params);
+        //combining the url to the query params
+        const stockQueryURL = `${stockURL}${queryString}`;
+        fetch(stockQueryURL)
         .then(response => {
             if (response.ok) {
                 return response.json();
@@ -103,101 +94,108 @@ function getStockData (searchSymbol) {
         .then(responseJson => {
             $('#js-stock-results').empty();
             $('#js-error-message').empty();
+            if (responseJson['Meta Data'] == null) {
+                $('#js-error-message').text('Reached max number of request.')
+            }
+            currentData = responseJson;
+            updateChart(responseJson);
             displayStockResults(responseJson);
             $('#js-symbol-search').val('');
-            return responseJson;
+            $('#js-footer').removeClass('hidden');
+            $('#js-flex-home-page').css('margin-top','20px');
             
         })
         .catch(error => {
             $('#js-error-message').text(`Something went wrong: ${error.message}`);
             $('#js-stock-results').empty();
         });
-    
+        console.log(currentData);
+        
+    } else {
+        updateChart(currentData);
+    }
 }
 
 //display stock symbol
-function displayStockResults (responseJson) {
-    // const stockSymbol = responseJson[metaData][symbol];
-    // console.log(stockSymbol);
+function displayStockResults (currentData) {
     $('#js-stock-results').append(
-        `<h2>${stockSymbol(responseJson)}</h2>
-        <table>
+        `<h2>${stockSymbol(currentData)}</h2>
+        <table class="stock-table">
             <tr>
                 <th>Open</th>
-                <td>${dailyOpen(responseJson)}</td>
+                <td>${dailyOpen(currentData)}</td>
             </tr>
             <tr>
                 <th>High</th>
-                <td>${parseFloat(dailyHigh(responseJson))}</td>
+                <td>${parseFloat(dailyHigh(currentData))}</td>
             </tr>
             <tr>
                 <th>Low</th>
-                <td>${dailyLow(responseJson)}</td>
+                <td>${dailyLow(currentData)}</td>
             </tr>
             <tr>
                 <th>52wk High</th>
-                <td>${parseFloat(fiftyTwoWeekHigh(responseJson))}</td>
+                <td>${parseFloat(fiftyTwoWeekHigh(currentData))}</td>
             </tr>
             <tr>
                 <th>52wk Low</th>
-                <td>${parseFloat(fiftyTwoWeekLow(responseJson))}</td>
+                <td>${parseFloat(fiftyTwoWeekLow(currentData))}</td>
             </tr>
             <tr>
                 <th>Volume</th>
-                <td>${stockVolume(responseJson)}</td>
+                <td>${stockVolume(currentData)}</td>
             </tr>
         </table>`
     )
 }
 
 //functoin to return stock Symbol
-function stockSymbol (responseJson) {
+function stockSymbol (currentData) {
     const metaData = 'Meta Data';
-    const symbol = '2. Symbol'
-    console.log(responseJson);
-    return responseJson[metaData][symbol];
+    const symbol = '2. Symbol';
+    return currentData[metaData][symbol];
 }
 
 //function to return daily open
-function dailyOpen (responseJson) {
+function dailyOpen (currentData) {
     const dailyAdjusted = 'Time Series (Daily)';
-    const keysDailyAdjusted = Object.keys(responseJson[dailyAdjusted]);
+    const keysDailyAdjusted = Object.keys(currentData[dailyAdjusted]);
     const dailyStock = keysDailyAdjusted.slice(0,1);
     const openStock = '1. open';
     
-    return responseJson[dailyAdjusted][dailyStock][openStock];
+    return currentData[dailyAdjusted][dailyStock][openStock];
 }
 
 //function to return daily high
-function dailyHigh (responseJson) {
+function dailyHigh (currentData) {
     const dailyAdjusted = 'Time Series (Daily)';
-    const keysDailyAdjusted = Object.keys(responseJson[dailyAdjusted]);
+    const keysDailyAdjusted = Object.keys(currentData[dailyAdjusted]);
     const dailyStock = keysDailyAdjusted.slice(0,1);
     const highStock = '2. high';
     
-    return responseJson[dailyAdjusted][dailyStock][highStock];
+    return currentData[dailyAdjusted][dailyStock][highStock];
 }
 
 //function to return daily low
-function dailyLow (responseJson) {
+function dailyLow (currentData) {
     const dailyAdjusted = 'Time Series (Daily)';
-    const keysDailyAdjusted = Object.keys(responseJson[dailyAdjusted]);
+    const keysDailyAdjusted = Object.keys(currentData[dailyAdjusted]);
     const dailyStock = keysDailyAdjusted.slice(0,1);
     const lowStock = '3. low';
 
-    return responseJson[dailyAdjusted][dailyStock][lowStock];
+    return currentData[dailyAdjusted][dailyStock][lowStock];
 }
 
 //funtion to return 52wk high
-function fiftyTwoWeekHigh (responseJson) {
+function fiftyTwoWeekHigh (currentData) {
     const weeklyAdjusted = 'Time Series (Daily)';
-    const keysWeeklyAdjusted = Object.keys(responseJson[weeklyAdjusted]);
+    const keysWeeklyAdjusted = Object.keys(currentData[weeklyAdjusted]);
     const fiftyTwoWeeks = keysWeeklyAdjusted.slice(0,365);
     const highStock = '4. close';
 
     const stockArray = [];
     for (let i = 0; i < fiftyTwoWeeks.length; i++) {
-        stockArray.push(responseJson[weeklyAdjusted][fiftyTwoWeeks[i]][highStock]);
+        stockArray.push(currentData[weeklyAdjusted][fiftyTwoWeeks[i]][highStock]);
     }
     
     stockArray.sort(function(a, b) { return a - b });
@@ -205,157 +203,89 @@ function fiftyTwoWeekHigh (responseJson) {
 }
 
 //function to return 52wk low
-function fiftyTwoWeekLow (responseJson) {
+function fiftyTwoWeekLow (currentData) {
     const weeklyAdjusted = 'Time Series (Daily)';
-    const keysWeeklyAdjusted = Object.keys(responseJson[weeklyAdjusted]);
+    const keysWeeklyAdjusted = Object.keys(currentData[weeklyAdjusted]);
     const fiftyTwoWeeks = keysWeeklyAdjusted.slice(0,365);
     const lowStock = '3. low';
-
     const stockLow = [];
 
     for (let i = 0; i < fiftyTwoWeeks.length; i++) {
-        stockLow.push(responseJson[weeklyAdjusted][fiftyTwoWeeks[i]][lowStock]);
+        stockLow.push(currentData[weeklyAdjusted][fiftyTwoWeeks[i]][lowStock]);
     }
     
     stockLow.sort((a,b) => a-b);
-
     return stockLow[0];
 }
 
 //function to return the volume of the stock
-function stockVolume (responseJson) {
+function stockVolume (currentData) {
     const dailyAdjusted = 'Time Series (Daily)';
-    const keysDailyAdjusted = Object.keys(responseJson[dailyAdjusted]);
+    const keysDailyAdjusted = Object.keys(currentData[dailyAdjusted]);
     const dailyStock = keysDailyAdjusted.slice(0,1);
     const volume = '6. volume';
 
-    return responseJson[dailyAdjusted][dailyStock][volume];
+    return currentData[dailyAdjusted][dailyStock][volume];
 }
 
+function updateChart (currentData) {
+    //stock symbol
+    const metaData = 'Meta Data';
+    const symbol = '2. Symbol';
+    const symbolStock = currentData[metaData][symbol];    
+    const dailyAdjusted = 'Time Series (Daily)';
+    const weekObject = Object.keys(currentData[dailyAdjusted]);
+    const stockClose = '4. close';
+    //Current week array
+    const chartDaily = weekObject.slice(0,30);
+    const stockPriceArray = [];
 
-//const symbol = 'aapl';
+    for (let i = 0; i < chartDaily.length; i++) {
+        stockPriceArray.push(currentData[dailyAdjusted][chartDaily[i]][stockClose]);
+    }
+
+    for (i = 0; i < stockPriceArray.length; i++) {
+        stockPriceArray[i] = parseFloat(stockPriceArray[i]);
+    }
+
+    const arrayHolder = [];
+    arrayHolder.push(['Week','Stock']);
+    for (i = 30; i >= 0; i--) {
+        arrayHolder.push([chartDaily[i], stockPriceArray[i]]);
+    }
+
+    const data = google.visualization.arrayToDataTable(arrayHolder);
+
+    const options = {
+        title: `${symbolStock}'s 30 days stock performance`,
+        backgroundColor: '#AEADAD',
+        width: '100%',
+        height: 400,
+        curveType: 'function',
+        legend: { position: 'bottom' }
+    };
+        
+    const chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+        
+    chart.draw(data, options);
+}
+
 //function to get stock symbol
 function stockForm() {
     $('form').submit( event => {
         event.preventDefault();
         const stock = $('#js-symbol-search').val();
-        google.charts.load('current', {packages: ['corechart', 'line']});
-        google.charts.setOnLoadCallback(drawLineColors(stock));
-    });
-}
-
-    //copy and paste google function into function
-    //Stock Chart
-
-
-function drawLineColors (stock) {
-    const stockURL = `https://www.alphavantage.co/query?`;
-    
-    //create parameters object
-    const params = {
-        function : 'TIME_SERIES_DAILY_ADJUSTED',
-        symbol : stock,
-        apikey : '2ROPO489EWWVNW2W'
-    }
-        //
-        //connecting params object to the formatStockQueryParans function
-    const queryString = formatStockQueryParams(params);
-    
-        //combining the url to the query params
-    const stockQueryURL = `${stockURL}${queryString}`;
-    fetch(stockQueryURL)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            $('#js-stock-results').empty();
-            throw new Error('Symbol not Found.');
-        })
-        .then(responseJson => {
-                //new array for X and Y values
-
-                //collect the date  first 4 digits
-                //collect the close data 
-
-                // adammain
-
-            const stockData = responseJson;
-            const dailyAdjusted = 'Time Series (Daily)';
-            const weekObject = Object.keys(stockData[dailyAdjusted]);
-            const stockClose = '4. close';
-                //Current week array
-            const chartDaily = weekObject.slice(0,30);
-            const stockPriceArray = [];
-
-
-            for (let i = 0; i < chartDaily.length; i++) {
-                stockPriceArray.push(stockData[dailyAdjusted][chartDaily[i]][stockClose]);
-            }
-
-
-            for (i = 0; i < stockPriceArray.length; i++) {
-                stockPriceArray[i] = parseFloat(stockPriceArray[i]);
-            }
-            const data = google.visualization.arrayToDataTable([
-                ['Week', 'Stock'],
-                [chartDaily[30],  stockPriceArray[30]],
-                [chartDaily[29],  stockPriceArray[29]],
-                [chartDaily[28],  stockPriceArray[28]],
-                [chartDaily[27],  stockPriceArray[27]],
-                [chartDaily[26],  stockPriceArray[26]],
-                [chartDaily[25],  stockPriceArray[25]],
-                [chartDaily[24],  stockPriceArray[24]],
-                [chartDaily[23],  stockPriceArray[23]],
-                [chartDaily[22],  stockPriceArray[22]],
-                [chartDaily[21],  stockPriceArray[21]],
-                [chartDaily[20],  stockPriceArray[20]],
-                [chartDaily[19],  stockPriceArray[19]],
-                [chartDaily[18],  stockPriceArray[18]],
-                [chartDaily[17],  stockPriceArray[17]],
-                [chartDaily[16],  stockPriceArray[16]],
-                [chartDaily[15],  stockPriceArray[15]],
-                [chartDaily[14],  stockPriceArray[14]],
-                [chartDaily[13],  stockPriceArray[13]],
-                [chartDaily[12],  stockPriceArray[12]],
-                [chartDaily[11],  stockPriceArray[11]],
-                [chartDaily[10],  stockPriceArray[10]],
-                [chartDaily[9],   stockPriceArray[9]],
-                [chartDaily[8],   stockPriceArray[8]],
-                [chartDaily[7],   stockPriceArray[7]],
-                [chartDaily[6],   stockPriceArray[6]],
-                [chartDaily[5],   stockPriceArray[5]],
-                [chartDaily[4],   stockPriceArray[4]],
-                [chartDaily[3],   stockPriceArray[3]],
-                [chartDaily[2],   stockPriceArray[2]],
-                [chartDaily[1],   stockPriceArray[1]],
-                [chartDaily[0],   stockPriceArray[0]]
-            ]);
-            const options = {
-                title: `${stock}'s Stock Performance`,
-                curveType: 'function',
-                legend: { position: 'bottom' }
-            };
-        
-            const chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-        
-            chart.draw(data, options);
-        })
-        .catch(error => {
-            $('#js-error-message').text(`Something went wrong: ${error.message}`);
-            $('#js-stock-results').empty();
-        });
-}
-
-function watchForm () {
-    $('form').submit( event => {
-        
-        event.preventDefault();
-        const searchSymbol = $('#js-symbol-search').val();
-        getStockData(searchSymbol);
-        getNewsArticles(searchSymbol);
+        getNewsArticles(stock);
+        google.charts.load("current", {packages: ['corechart', 'line']});
+        google.charts.setOnLoadCallback(getStockData(stock));
         $('#results').removeClass('hidden');
     });
 }
 
 $(stockForm);
-$(watchForm);
+
+$(window).resize(function() {
+    const stock = $('#js-symbol-search').val();
+    console.log(currentData);
+    getStockData(stock);
+});
